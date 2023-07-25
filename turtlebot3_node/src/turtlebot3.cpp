304d06875095327340b17e472c769d1f1bf561d3
kijonggil <kkjong@robotis.com>
2023-07-25 15:28:51 +0900

    Added init param
    Signed-off-by: kijonggil <kkjong@robotis.com>

--- turtlebot3_node/src/turtlebot3.cpp
+++ turtlebot3_node/src/turtlebot3.cpp
@@ -57,10 +57,10 @@ void TurtleBot3::init_dynamixel_sdk_wrapper(const std::string & usb_port)
 {
   DynamixelSDKWrapper::Device opencr = {usb_port, 200, 1000000, 2.0f};
   RCLCPP_INFO(this->get_logger(), "test sss %s", usb_port.c_str());
-  this->declare_parameter<uint8_t>("opencr.id");
+  this->declare_parameter<uint8_t>("opencr.id", 200);
   RCLCPP_INFO(this->get_logger(), "222222 %s", usb_port.c_str());
-  this->declare_parameter<int>("opencr.baud_rate");
-  this->declare_parameter<float>("opencr.protocol_version");
+  this->declare_parameter<int>("opencr.baud_rate", 1000000);
+  this->declare_parameter<float>("opencr.protocol_version", 2.0f);
 
   this->get_parameter_or<uint8_t>("opencr.id", opencr.id, 200);
   this->get_parameter_or<int>("opencr.baud_rate", opencr.baud_rate, 1000000);
